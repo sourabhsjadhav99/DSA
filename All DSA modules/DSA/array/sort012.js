e0f0090e9c7c280535cb47e9a2d95a648c292751
sourabhsjadhav99 <sourabhsjadhav99@gmail.com>
2023-05-22 15:20:31 +0530

    palindrome LL

--- All DSA modules/DSA/array/sort012.js
+++ All DSA modules/DSA/array/sort012.js
@@ -1,34 +1,35 @@
 
-	// function sort012(a,arr_size){
-	// 	let lo = 0;
-	// 	let hi = arr_size - 1;
-	// 	let mid = 0;
-	// 	let temp = 0;
-	// 	while (mid <= hi){
-	// 		if(a[mid] == 0){
-	// 			temp = a[lo];
-	// 			a[lo] = a[mid];
-	// 			a[mid] = temp;
-	// 			lo++;
-	// 			mid++;
-	// 		}
-	// 		else if(a[mid] == 1){
-	// 			mid++;
-	// 		}
-	// 		else{
-	// 			temp = a[mid];
-	// 			a[mid] = a[hi];
-	// 			a[hi] = temp;
-	// 			hi--;
-	// 		}
-	// 	}
-    //     return a
-	// }
+	function sort012(a,arr_size){
+		let lo = 0;
+		let hi = arr_size - 1;
+		let mid = 0;
+		let temp = 0;
+		while (mid <= hi){
+			if(a[mid] == 0){
+				temp = a[lo];
+				a[lo] = a[mid];
+				a[mid] = temp;
+				lo++;
+				mid++;
+
+			}else if(a[mid] == 1){
+				mid++;
+				
+			}else{
+			
+				temp = a[mid];
+				a[mid] = a[hi];
+				a[hi] = temp;
+				hi--;
+			}
+		}
+        return a
+	}
 	
 
-	// let arr= [0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1 ];
-	// let arr_size = arr.length;
-	// console.log(sort012(arr, arr_size))
+	let arr= [0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1 ];
+	let arr_size = arr.length;
+	console.log(sort012(arr, arr_size))
 
 	
 // 	let arr= [0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1 ];
@@ -46,4 +47,4 @@
 // 	}
 // 	console.log(...arr2[0], ...arr2[1], ...arr2[2])
 
-// 	let arr= [0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1 ];
+	// let arr= [0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1 ];
